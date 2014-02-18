b5b0ea93d66276e3155683cdc93a1c47c958d8de
Kanbour Sama <samak@cmu.edu>
2014-02-18 13:29:16 -0500

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 KcwWorld::Application.routes.draw do
 
+  resources :articles
   resources :categories
   resources :proverbs
   resources :photos
-  resources :articles
     
   match 'home' => 'home#index', :as => :home
   root :to => 'home#index'
